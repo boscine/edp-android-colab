ad7e8f2e1b8ea9a7c687ec63336dfa89bb330ed2
boscine <jlmandahinog65115@liceo.edu.ph>
2026-07-07 01:59:58 +0800

    Initial commit with added Button

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+       modifier = modifier
+           .padding(16.dp)
+           .fillMaxWidth()
+           .background(MaterialTheme.colorScheme.onSurfaceVariant),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            fontFamily = FontFamily.Monospace,
+            color = Color.White,
+            text = "Hello $name",
+            textAlign = TextAlign.Center,
+            modifier = modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = { /* Do something */ },
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text("Click Me!")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
