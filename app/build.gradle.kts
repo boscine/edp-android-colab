2191e3c0a704f545a97a0deeccb92f6ee622774e
Kissie Ann Apple C. Aput <kaaput42160@liceo.edu.ph>
2026-07-13 13:37:40 +0800

    Initial commit: Business Card App
    # Conflicts:
    #	.idea/misc.xml
    #	app/build.gradle.kts
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/themes.xml
    #	gradle/libs.versions.toml
    #	gradle/wrapper/gradle-wrapper.properties
    #	settings.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,17 +4,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    namespace = "com.example.mandahinog"
+    compileSdk = 37
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.example.mandahinog"
         minSdk = 32
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
@@ -41,6 +37,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.material.icons.extended)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
